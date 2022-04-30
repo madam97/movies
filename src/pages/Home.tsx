39922df4a62d887e6ac659ca2765d08b4ead047d
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 11:45:50 +0200

    added loading top rated movies' poster on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../hooks/useRedux'
+import { getTopRatedMovies, selectPosters } from '../store/slices/movieSlice'
+
 export default function Home() {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getTopRatedMovies());
+  }, []);
+
+  const posters = useAppSelector(selectPosters);
+
+  console.log(posters);
+
+  // -----------------------------------------
+
   return (
     <main>Home</main>
   )
