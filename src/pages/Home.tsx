d3407761c0aeb9c2ff17d44d98fc9fc7c8511605
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 16:03:53 +0200

    changed Home to show movie list by using the list url param, the code now has comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
 import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import MoviePoster from '../components/MoviePoster';
 import { useAppDispatch, useAppSelector } from '../hooks/useRedux'
-import { getTopRatedMovies, selectPosters } from '../store/slices/movieSlice'
+import { getPopularMovies, getTopRatedMovies, selectPosters } from '../store/slices/movieSlice'
 
+/**
+ * The component of the Home page
+ * @returns {JSX.Element}
+ */
 export default function Home() {
+
+  /** Fetches the top rated or popular movies */
+  const [searchParams] = useSearchParams();
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getTopRatedMovies());
-  }, []);
+    const listType = searchParams.get('list');
 
+    if (!listType || listType === 'popular') {
+      dispatch(getPopularMovies());
+    } else if (listType === 'topRated') {
+      dispatch(getTopRatedMovies());
+    }
+  }, [searchParams]);
+
+  /** @const {IMoviePoster[]} posters The movie posters that will be displayed */
   const posters = useAppSelector(selectPosters);
-
-  console.log(posters);
 
   // -----------------------------------------
 
