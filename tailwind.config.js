f49f6b453ea8789606a9dd7ca91af4c7ceb93734
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 12:38:04 +0200

    changed style colors added min height class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    colors: {
+      'dark': '#051A37',
+      'mid': '#8194A5',
+      'light': '#DDE3E8',
+      'shade': '#F8F9FA',
+      'white': '#FFFFFF'
+    },
     fontFamily: {
       'body': ['Lato', 'sans-serif']
     },
+    minHeight: {
+      '60': '15rem'
+    }
   },
   plugins: [],
 }
