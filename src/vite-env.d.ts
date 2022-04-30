14b93d31a8bca0209fd4f5d9ea8aee8c015bdfb7
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 11:42:50 +0200

    added interface of env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MOVIEDB_API_BASE_URL: string,
+  readonly VITE_MOVIEDB_API_KEY: string,
+  readonly VITE_MOVIEDB_IMG_BASE_PATH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
