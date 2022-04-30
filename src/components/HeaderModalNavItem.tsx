261377d2dab53148c0a43b993d63ed234d650ec5
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 15:52:42 +0200

    changed HeaderModalNavItem to use handleClick prop

--- src/components/HeaderModalNavItem.tsx
+++ src/components/HeaderModalNavItem.tsx
@@ -9,14 +9,17 @@ type HeaderModalNavItemProps = {
   /** The subtitle */
   subtitle: string,
   /** The link's url */
-  to: string
+  to: string,
+  /** The function that runs after clicking on the link */
+  handleClick: (e: React.MouseEvent<HTMLAnchorElement>) => void
 }
 
-export default function HeaderModalNavItem({ className, title, subtitle, to }: HeaderModalNavItemProps) {
+export default function HeaderModalNavItem({ className, title, subtitle, to, handleClick }: HeaderModalNavItemProps) {
   return (
     <Link 
       className={`flex flex-col p-4 bg-dark rounded-xl text-white ${className}`} 
       to={to}
+      onClick={handleClick}
     >
       <span className="text-base font-bold">{title}</span>
       <span className="font-medium">{subtitle}</span>
