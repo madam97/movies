db48dc4ec785e982ac4e50b75a56655bbb977f3c
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 14:51:07 +0200

    added getPosterFromMovie, requestGetMovies and requestGetPopularMovies function into Movies model changed requestGetTopRatedMovies function to use requestGetMovies

--- src/models/movieModel.ts
+++ src/models/movieModel.ts
@@ -1,21 +1,41 @@
-import axios from "axios"
-import IMoviePoster from "../interfaces/IMoviePoster"
+import axios from 'axios'
+import IMoviePoster from '../interfaces/IMoviePoster'
+import IObject from '../interfaces/IObject'
 
 const BASE_URL = import.meta.env.VITE_MOVIEDB_API_BASE_URL;
 const KEY = import.meta.env.VITE_MOVIEDB_API_KEY;
 const IMG_BASE_PATH = import.meta.env.VITE_MOVIEDB_IMG_BASE_PATH;
 
 /**
- * Returns the top rated movies
+ * Returns the poster data from the given movie
+ * @param {IObject} movie 
+ * @returns {IMoviePoster | null}
+ */
+const getPosterFromMovie = (movie: IObject): IMoviePoster | null => {
+  if (movie.id && movie.title && movie.vote_average) {
+    return {
+      id: movie.id,
+      title: movie.title,
+      posterPath: movie.poster_path ? IMG_BASE_PATH+movie.poster_path : null,
+      voteAverage: movie.vote_average
+    } as IMoviePoster;
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Fetches the movies on the given endpoint and returns their poster data
+ * @param {'top_rated' | 'popular'} endpoint 
  * @returns {Promise<IMoviePoster[]>}
  */
-export const requestGetTopRatedMovies = async (): Promise<IMoviePoster[]> => {
+const requestGetMovies = async (endpoint: 'top_rated' | 'popular'): Promise<IMoviePoster[]> => {
   const posters: IMoviePoster[] = [];
 
   try {
     const resp = await axios.request({
       method: 'get',
-      url: BASE_URL+'/movie/top_rated',
+      url: BASE_URL+'/movie/'+endpoint,
       params: {
         api_key: KEY,
         language: 'en-US'
@@ -26,15 +46,10 @@ export const requestGetTopRatedMovies = async (): Promise<IMoviePoster[]> => {
 
     if (data.results) {
       for (let i = 0; i < data.results.length; ++i) {
-        const movie = data.results[i];
-
-        if (movie.id && movie.title && movie.vote_average) {
-          posters.push({
-            id: movie.id,
-            title: movie.title,
-            posterPath: movie.poster_path ? IMG_BASE_PATH+movie.poster_path : null,
-            voteAverage: movie.vote_average
-          } as IMoviePoster);
+        const movie = getPosterFromMovie(data.results[i]);
+    
+        if (movie) {
+          posters.push(movie);
         }
       }
     }
@@ -46,5 +61,18 @@ export const requestGetTopRatedMovies = async (): Promise<IMoviePoster[]> => {
   }
 }
 
+/**
+ * Returns the top rated movies' poster data
+ * @returns {Promise<IMoviePoster[]>}
+ */
+export const requestGetTopRatedMovies = (): Promise<IMoviePoster[]> => {
+  return requestGetMovies('top_rated');
+}
 
-import.meta.env.VITE_MOVIEDB_API_BASE_URL
+/**
+ * Returns the popular movies' poster data
+ * @returns {Promise<IMoviePoster[]>}
+ */
+export const requestGetPopularMovies = (): Promise<IMoviePoster[]> => {
+  return requestGetMovies('popular');
+}
