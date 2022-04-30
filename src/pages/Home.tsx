76e2aede46ef4b1363862a88f1377b6ddf2e403a
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 12:39:13 +0200

    added content of Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import MoviePoster from '../components/MoviePoster';
 import { useAppDispatch, useAppSelector } from '../hooks/useRedux'
 import { getTopRatedMovies, selectPosters } from '../store/slices/movieSlice'
 
@@ -17,6 +18,15 @@ export default function Home() {
   // -----------------------------------------
 
   return (
-    <main>Home</main>
+    <main>
+      <section className="px-4 pt-14 pb-6 bg-white">
+        <h1 className="mb-1.5 text-2xl text-dark font-extrabold">Hello Verycreatives</h1>
+        <p className="pl-4 font-semibold">Check these movies out</p>
+      </section>
+
+      <section className="grid gap-x-1 gap-y-2 grid-cols-2 px-2 py-3">
+        {posters.map(poster => <MoviePoster key={poster.id} poster={poster} />)}
+      </section>
+    </main>
   )
 }
