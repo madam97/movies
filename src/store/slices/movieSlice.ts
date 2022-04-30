d13044d56e24017360584e6c833702b252638b10
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 18:29:50 +0200

    added addFavMovie and removeFavMovie reducers

--- src/store/slices/movieSlice.ts
+++ src/store/slices/movieSlice.ts
@@ -5,12 +5,14 @@ import { TRootState } from '../configureStore'
 
 interface IMovieState {
   posters: IMoviePoster[],
-  movieDetailed: IMovie | null
+  movieDetailed: IMovie | null,
+  favMovies: IMovie[]
 }
 
 const initialState: IMovieState = {
   posters: [],
-  movieDetailed: null
+  movieDetailed: null,
+  favMovies: []
 }
 
 const movieSlice = createSlice({
@@ -35,13 +37,51 @@ const movieSlice = createSlice({
         ...state, 
         movieDetailed
       };
+    },
+
+    addFavMovie(state: IMovieState, action: PayloadAction<IMovie>) {
+      const movie = action.payload;
+
+      if (!state.favMovies.find(favMovie => favMovie.id === movie.id)) {
+        return { 
+          ...state, 
+          favMovies: [
+            ...state.favMovies,
+            movie
+          ]
+        };
+      } else {
+        return state;
+      }
+    },
+
+    removeFavMovie(state: IMovieState, action: PayloadAction<number>) {
+      const id = action.payload;
+
+      const index = state.favMovies.findIndex(favMovie => favMovie.id === id);
+
+      if (index > -1) {
+        const favMovies = state.favMovies.slice();
+        favMovies.splice(index, 1);
+
+        return { 
+          ...state, 
+          favMovies: [
+            ...favMovies
+          ]
+        };
+      } else {
+        return state;
+      }
     }
   }
 });
 
-export const { getTopRatedMovies, getPopularMovies, getMovieDetailed, setPosters, setMovieDetailed } = movieSlice.actions;
+export const { getTopRatedMovies, getPopularMovies, getMovieDetailed, setPosters, setMovieDetailed, addFavMovie, removeFavMovie } = movieSlice.actions;
 
 export const selectPosters = (state: TRootState) => state.movie.posters;
 export const selectMovieDetailed = (state: TRootState) => state.movie.movieDetailed;
+export const selectFavMovies = (state: TRootState) => state.movie.favMovies;
+export const isFavMovie = (id: number) => (state: TRootState) => state.movie.favMovies.findIndex(favMovie => favMovie.id === id) > -1;
 
 export default movieSlice.reducer;
