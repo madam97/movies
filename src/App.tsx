609f5cbab94997d0a9ad8eb798dc95b308535f49
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-30 17:57:38 +0200

    changed App so Header and Footer components are hidden on movie detailed view added header in MovieDetailed page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Header from './pages/Header'
 import Footer from './pages/Footer'
 import Home from './pages/Home'
@@ -7,9 +8,30 @@ import MovieDetailed from './pages/MovieDetailed'
 import ErrorPage from './pages/ErrorPage'
 
 function App() {
+
+  const location = useLocation();
+
+  /**
+   * Returns true if the current page is movies detailed page
+   * @returns {boolean}
+   */
+  const isDetailedView = (): boolean => {
+    return /^\/movies\/[0-9]+$/.test(location.pathname);
+  }
+
+  /** @const {boolean} detailedView If true, the footer will be hidden and the header will have other appearence */
+  const [detailedView, setDetailedView] = useState<boolean>(isDetailedView);
+
+  /** Updates the value of detailedView state */
+  useEffect(() => {
+    setDetailedView(isDetailedView);
+  }, [location]);
+
+  // --------------------------------------------
+
   return (
     <div className="bg-shade text-mid text-sm font-body">
-      <Header />
+      {!detailedView && <Header />}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +40,7 @@ function App() {
         <Route path="*" element={<ErrorPage />} />
       </Routes>
 
-      <Footer />
+      <Footer show={!detailedView} />
     </div>
   )
 }
--- src/pages/MovieDetailed.tsx
+++ src/pages/MovieDetailed.tsx
@@ -1,51 +1,73 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faHeart as faHeartSolid, faStar } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 import { useAppDispatch, useAppSelector } from '../hooks/useRedux'
 import { getMovieDetailed, selectMovieDetailed } from '../store/slices/movieSlice';
 import Image from '../components/Image';
+import HeaderContainer from '../components/HeaderContainer';
 
 export default function MovieDetailed() {
+
+  const navigate = useNavigate();
 
   const { id } = useParams();
 
   const dispatch = useAppDispatch();
 
+  /** Loads the movie */
   useEffect(() => {
     if (id) {
       dispatch(getMovieDetailed(parseInt(id)));
     }
   }, [id]);
 
+  /** @const {IMovie} movie The movie, its data will appears */
   const movie = useAppSelector(selectMovieDetailed);
 
   // -----------------------------------------
 
   return (
-    <main className="min-h-screen bg-white">
-      {movie && 
-        <section>
-          <Image src={movie.backdropPath} alt={movie.title} />
+    <>
+      <HeaderContainer>
+        <button className="flex items-center mr-2.5" onClick={() => navigate(-1)}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
 
-          <div className="relative -mt-3 px-4 pt-6 pb-4.5 bg-white rounded-t-xl">
-            <h1 className="text-2xl text-dark font-extrabold">
-              {movie.title}
-              {movie.originalTitle && movie.title !== movie.originalTitle ? ' ('+movie.originalTitle+')' : ''}
-            </h1>
-            {movie.tagline && <h2 className="text-md text-dark font-semibold">{movie.tagline}</h2>}
+        <span className="mr-auto font-bold">
+          Movie Details
+        </span>
 
-            <div className="mt-3 mb-8 text-xs font-medium">
-              <span>
-                <FontAwesomeIcon className="mr-1" icon={faStar} />
-                {movie.voteAverage}
-              </span>
+        <button className="flex items-center">
+          <FontAwesomeIcon icon={faHeartRegular} />
+        </button>
+      </HeaderContainer>
+
+      <main className="min-h-screen bg-white">
+        {movie && 
+          <section>
+            <Image src={movie.backdropPath} alt={movie.title} />
+
+            <div className="relative -mt-3 px-4 pt-6 pb-4.5 bg-white rounded-t-xl">
+              <h1 className="text-2xl text-dark font-extrabold">
+                {movie.title}
+                {movie.originalTitle && movie.title !== movie.originalTitle ? ' ('+movie.originalTitle+')' : ''}
+              </h1>
+              {movie.tagline && <h2 className="text-md text-dark font-semibold">{movie.tagline}</h2>}
+
+              <div className="mt-3 mb-8 text-xs font-medium">
+                <span>
+                  <FontAwesomeIcon className="mr-1" icon={faStar} />
+                  {movie.voteAverage}
+                </span>
+              </div>
+
+              <p className="text-sm text-dark">{movie.overview}</p>
             </div>
-
-            <p className="text-sm text-dark">{movie.overview}</p>
-          </div>
-        </section>
-      }
-    </main>
+          </section>
+        }
+      </main>
+    </>
   )
 }
