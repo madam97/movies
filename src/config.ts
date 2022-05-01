853f25ab7f05c52bbc891d4f6ed976d2c9c6b1ca
Mézes Ádám <mezesadam1997@gmail.com>
2022-05-01 12:02:08 +0200

    renamed TBreakpoints to TBreakpoint in config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-import TBreakpoints from './types/TBreakpoints'
+import TBreakpoint from './types/TBreakpoint'
 
-export const defBreakpoints: Record<TBreakpoints, string> = {
+export const defBreakpoints: Record<TBreakpoint, string> = {
   tablet: import.meta.env.VITE_BREAKPOINT_TABLET,
   laptop: import.meta.env.VITE_BREAKPOINT_LAPTOP,
   desktop: import.meta.env.VITE_BREAKPOINT_DESKTOP
